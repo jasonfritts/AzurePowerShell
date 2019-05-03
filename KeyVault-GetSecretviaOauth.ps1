ce4d4e50ff17b2a5218682bd594f8e604eb90ae0
jasonfritts <38323403+jasonfritts@users.noreply.github.com>
2019-05-03 13:51:42 -0400

    Update KeyVault-GetSecretviaOauth.ps1

--- KeyVault-GetSecretviaOauth.ps1
+++ KeyVault-GetSecretviaOauth.ps1
@@ -1,8 +1,8 @@
-$ClientID       = "a0eb5148-fa60-4b20-a4e9-a1d0bf2bd284"       # Insert your application's Client ID, a Globally Unique ID (registered by Global Admin)
-$ClientSecret   = "607eddfb-6561-4a13-9933-3f86ca352cf4"   # Insert your application's Client Key/Secret string
+$ClientID       = "0c5e5210-a4a8-4b59-9377-2dc739711496"       # Insert your application's Client ID, a Globally Unique ID (registered by Global Admin)
+$ClientSecret   = "MDHVLwO8bE/XnNXXxoXcGz5pfsN4qauAGQab75Tr6FQ="   # Insert your application's Client Key/Secret string
 $loginURL       = "https://login.microsoftonline.com"     # AAD Instance; for example https://login.microsoftonline.com
-$tenantdomain = "jasonfritts.onmicrosoft.com"  # AAD tenant ID
-$vaultUri = "https://kvfritts2.vault.azure.net/secrets/Secret1/0193486d28034d35bf8334842d3efe0f?api-version=2016-10-01"   # Key Vault resource URI, include version
+$tenantdomain = "91ceb514-5ead-468c-a6ae-048e103d57f0"
+$keyvaultURI = "https://frittssanvault.vault.azure.net/secrets/secret/2de37f8ea5534eae8095fe2e74fa0df5?api-version=2016-10-01"
 
 # Create HTTP header, get an OAuth2 access token based on client id, secret and tenant domain, change resource URL to match env. endpoint
 $body       = @{grant_type="client_credentials";client_id=$ClientID;client_secret=$ClientSecret;resource="https://vault.azure.net"}
@@ -12,6 +12,6 @@ $oauth2     = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2
 
 #Change Keyvault URL to match your vault
 $KeyVaultToken = $oauth2.access_token
-$result = (Invoke-WebRequest -Uri $vaulturi -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}).content
+$result = (Invoke-WebRequest -Uri $keyvaultURI -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}).content
 $result = $result | ConvertFrom-Json
 $result.value
